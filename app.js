815ad53b2c677bf8a9c531be1521a80a453912a1
migueloruiz <red2000ace@gmail.com>
2017-01-04 12:14:01 -0600

    Cambia de path public

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.use(favicon(path.join(__dirname, 'src/public', 'favicon.ico')))
 app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'src/public')))
 
 var server = http.createServer(app)
 server.on('error', onError)
